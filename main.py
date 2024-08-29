a2eff58e5fc482c93cdbdb0caea440494a545c7b
MandonSeapoe <mseapoe1@gmail.com>
2024-08-29 11:46:03 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+nane = "Georgia"
+age = 3.5
+hair_color = "brown"
+
+print(name)
+
+def hair_color_finction():    else
+   print("you don't have yellow hair")
+   
+   
+   hair_color_funtion
+   
+   
+   
+   
+   #now conatenate the name and the age and print it out
